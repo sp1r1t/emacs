523b6d005995af8e305a765fd30fdd2f2bb1d992
Julian Konrath <julian.konrath@gmail.com>
2021-03-03 09:28:24 +0100

    change primary monitor

--- .screenlayout/desk-setup.sh
+++ .screenlayout/desk-setup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 3840x0 --rotate right --output HDMI-A-0 --mode 3840x2160 --pos 0x200 --rotate normal --output DVI-D-0 --off
+xrandr --output DisplayPort-0 --mode 2560x1440 --pos 3840x0 --rotate right --output HDMI-A-0 --primary --mode 3840x2160 --pos 0x200 --rotate normal --output DVI-D-0 --off
